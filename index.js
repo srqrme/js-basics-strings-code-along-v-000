3e6e1e011dcd47090a5de54de6b580f459e31390
Sharmaine Requerme <srqrme@gmail.com>
2019-07-19 19:33:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = welcomeMessage.slice(0, 22) + '!';
+const shortGreeting = welcomeMessage.slice(0, 22);
